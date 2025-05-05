d696f03066fdd04b49af34ff72428fac0e4f82fe
Amir Talib <amirtalib@Amirs-MacBook-Pro.local>
2025-05-05 01:59:28 +0100

    Update navbar with contact modal and improve UI

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,8 @@ import { defineConfig } from "vite";
 import jsconfigPaths from "vite-jsconfig-paths";
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/metrosource/',
   plugins: [react(), jsconfigPaths(), tailwindcss()],
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]'
-      }
-    }
-  },
-  publicDir: 'public'
 });
